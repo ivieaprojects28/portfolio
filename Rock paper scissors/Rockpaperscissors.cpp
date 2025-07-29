3f7244cb6ad435e9df25bbc4cc534fd6134512b1
ivieaprojects28 <ivie.avwenagha@outlook.com>
2025-07-29 15:51:17 +0100

    fixing the logic errors: complete!

--- Rock paper scissors/Rockpaperscissors.cpp
+++ Rock paper scissors/Rockpaperscissors.cpp
@@ -8,37 +8,58 @@ using namespace std;
 
 int main () {
     srand(time(0));
-    
+    int result = 0;  
     int choice;
     string says;
     choice = (rand()%3)+1; //output between 1 and 3
 
     switch (choice) {
         case 1:
-            says = "Rock";
+            says = "rock";
             break;
         case 2:
-            says = "Paper";
+            says = "paper";
             break;
         case 3:
-            says = "Scissors";
+            says = "scissors";
             break;
     }
     string user_choice;
-    cout <<"Rock...paper..."<<endl;
+    cout <<"Rock...paper... (say your answer now)"<<endl;
     cin >>user_choice;
     cout << says<< "!"<< endl;
 
 
     //analysing combination of answers
-
-    if (user_choice == says) {
-        cout <<"Wow, the same answer! Let's go again!"<<endl;
-    } else if (says == "Rock" && user_choice == "Paper") {
-        cout <<"Paper beats rock, so you win :(. Go again?"<<endl;
-    } else if (says == "Rock" && user_choice == "Scissors") {
-        cout << "Rock beats scissors, so I win! Haha! >:)"<<endl;
+    if (user_choice == says){
+        result = 0;
+        cout <<"Oh! O-O. We said the same thing. Go again?";
     }
+    if (user_choice == "scissors" && says == "paper") {
+        result = 1;
+    }
+    if (user_choice == "scissors" && says == "rock") {
+        result = -1;
+    }
+    if (user_choice == "paper" && says == "rock") {
+        result = 1;
+    }
+    if (user_choice == "paper" && says == "scissors") {
+        result = -1;
+    }
+    if (user_choice == "rock" && says == "paper") {
+        result = -1;
+    }
+    if (user_choice == "rock" && says == "scissors") {
+        result = 1;
+    }
+
+    if (result == -1) {
+        cout <<"I win! Good game! Should we play again?"<<endl;
+        }
+    else if (result = 1){
+        cout <<"Aww, you win. Good game! Should we play again?";
+        } 
 
     return 0;
 }
